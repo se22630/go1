259e92496d1fd4f5688caa8a6aaaefe9818f01e4
se22630 <m314will@users.noreply.github.com>
2023-09-28 23:59:12 +0100

    Complete

--- gol/gol.go
+++ gol/gol.go
@@ -1,48 +1,71 @@
 package main
 
-import "fmt"
-
-func neighbour(p golParams, x, y int, world [][]byte) int {
+func neighbour(p golParams, world [][]byte, x, y int) int {
 	count := 0
-	for i := -1; i < 1; i++ {
-		for j := -1; j < 1; j++ {
-			if i != 0 && j != 0 && x+i > -1 && x+i < p.imageWidth && y+j > -1 && y+j < p.imageHeight {
-				if world[x+i][y+j] != 0 {
-					fmt.Println(world[x+i][y+j])
-				}
-				if world[x+i][y+j] == 255 {
-					count++
-				}
+	if x != 0 {
+		if world[x-1][y] == 255 {
+			count++
+		}
+		if y != 0 {
+			if world[x-1][y-1] == 255 {
+				count++
 			}
+		}
+		if y != p.imageHeight-1 {
+			if world[x-1][y+1] == 255 {
+				count++
+			}
+		}
+	}
+	if x != p.imageWidth-1 {
+		if world[x+1][y] == 255 {
+			count++
+		}
+		if y != 0 {
+			if world[x+1][y-1] == 255 {
+				count++
+			}
+		}
+		if y != p.imageHeight-1 {
+			if world[x+1][y+1] == 255 {
+				count++
+			}
+		}
+	}
+	if y != 0 {
+		if world[x][y-1] == 255 {
+			count++
+		}
+	}
+	if y != p.imageHeight-1 {
+		if world[x][y+1] == 255 {
+			count++
 		}
 	}
 	return count
+
 }
 
 func calculateNextState(p golParams, world [][]byte) [][]byte {
-	var staticWorld = world
-	for i := 0; i < p.imageWidth; i++ {
+	staticWorld := world
+	for i := 0; i < p.imageWidth-1; i++ {
 		for j := 0; j < p.imageHeight; j++ {
-			liveNeighbour := neighbour(p, i, j, staticWorld)
-			if staticWorld[i][j] == 0 {
-				if liveNeighbour == 3 {
-					world[i][j] = 255
-				}
-			}
+			lives := neighbour(p, staticWorld, i, j)
 			if staticWorld[i][j] == 255 {
-				fmt.Println(i, j)
-				if liveNeighbour == 2 || liveNeighbour == 3 {
+				if lives == 2 || lives == 3 {
 					world[i][j] = 255
 				}
-				if liveNeighbour < 2 || liveNeighbour > 3 {
+				if lives < 2 || lives > 3 {
 					world[i][j] = 0
 				}
 			}
-
+			if staticWorld[i][j] == 0 {
+				if lives == 3 {
+					world[i][j] = 255
+				}
+			}
 		}
-
 	}
-
 	return world
 
 }
