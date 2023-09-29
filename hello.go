43fcca8fc0cb7f199b68a8e2a7f0c8283ce2cd68
se22630 <m314will@users.noreply.github.com>
2023-09-29 13:09:30 +0100

    implemented for loop

--- hello.go
+++ hello.go
@@ -5,19 +5,15 @@ import (
 	"time"
 )
 
-func say(s string, c chan int) {
-	fmt.Println(s, c)
+func say(s int, c chan int) {
+	fmt.Println("HELLO", s)
 	time.Sleep(1 * time.Second)
 }
 
 func main() {
 	chan1 := make(chan int)
-	chan2 := make(chan int)
-	chan3 := make(chan int)
-	chan4 := make(chan int)
-	go say("hello 1", chan1)
-	go say("hello 2", chan2)
-	go say("hello 3", chan3)
-	go say("hello 4", chan4)
+	for i := 1; i < 6; i++ {
+		go say(i, chan1)
+	}
 	time.Sleep(1 * time.Second)
 }
